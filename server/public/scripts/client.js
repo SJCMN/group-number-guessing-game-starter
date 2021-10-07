b1f0868795e0d28fae6aab88a02cedc0b67959ae
Kongxib Vue <kongcvue@gmail.com>
2021-10-07 12:02:26 -0500

    started up html and laid out frame

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,14 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  // click listeners
+  $('#submitButton').on('click', playRound )
+
+
+}
+
+function playRound(){
+
+  
 }
